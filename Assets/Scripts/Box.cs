fc12da60c0823beb625c05f433fdb05fff98edcf
mfahmi0304 <m.fahminuraziz93@gmail.com>
2021-10-12 06:03:40 +0700

    [UPDATE] Problem 6

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    public Vector2 scaleRandomValue;
+
+    private Transform player;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<MouseControl>().transform;
+    }
+
+
+    public void Spawn()
+    {
+        bool boxSpawned = false;
+        while (!boxSpawned)
+        {
+            Vector2 spawnPosition = GameManager.Instance.GetRandomPosition();
+            if (((Vector2)player.position - spawnPosition).magnitude < 2)
+            {
+                continue;
+            }
+            else
+            {
+                transform.position = spawnPosition;
+                Setup();
+                boxSpawned = true;
+            }
+        }
+    }
+
+    private void Setup()
+    {
+        float xScale = Random.Range(scaleRandomValue.x, scaleRandomValue.y);
+        float yScale = Random.Range(scaleRandomValue.x, scaleRandomValue.y);
+
+        transform.localScale = new Vector2(xScale, yScale);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+                if (_instance == null) Debug.LogError(" Game Manager Not Found!!!");
+            }
+            return _instance;
+        }
+    }
+    #endregion
+
+    [Header("Box Controller")]
+    public int boxSpawn;
+    [SerializeField] Box boxPrefabs;
+
+    [Header("Game Area Constraint")]
+    public float areaConstraintValue = 5f;
+
+    private void Start()
+    {
+        for (int i = 0; i < boxSpawn; i++)
+        {
+            Box box = Instantiate(boxPrefabs);
+            box.Spawn();
+        }
+    }
+
+    public Vector2 GetRandomPosition()
+    {
+        float xPosition = Random.Range(-areaConstraintValue, areaConstraintValue);
+        float yPosition = Random.Range(-areaConstraintValue, areaConstraintValue);
+
+        return new Vector2(xPosition, yPosition);
+    }
+}
